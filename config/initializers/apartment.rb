c07133d9ca7274e2d463def6eb0d4d63cb751575
Elizeu Santos <elizeusdsantos@gmail.com>
2020-05-15 19:39:24 -0300

    Ignores app domain on heroku

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -108,5 +108,5 @@ Rails.application.config.middleware.use Apartment::Elevators::Subdomain
 # Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
 # Rails.application.config.middleware.use Apartment::Elevators::Host
 
-Apartment::Elevators::Subdomain.excluded_subdomains = ['www', 'public']
+Apartment::Elevators::Subdomain.excluded_subdomains = ['www', 'public', 'floating-lowlands-78795']
 
