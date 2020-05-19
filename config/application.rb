899f4c72c080158e407b47b12b60af288304a638
Elizeu Santos <elizeusdsantos@gmail.com>
2020-05-18 21:40:15 -0300

    Prevents generation of stub code

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,13 @@ module NicChallenge
   class Application < Rails::Application
     config.load_defaults 5.2
     config.middleware.use Apartment::Elevators::Subdomain
+
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
   end
 end
